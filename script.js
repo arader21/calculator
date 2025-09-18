733513f9c8185e10a0fb696f3d1128cef98d6745
arader21 <austinrader21@gmail.com>
2025-09-18 16:34:01 -0600

    script added, clear button, calculate.

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ display.value += input;
 function appendOperation(input) {
     display.value += input;
 }
+function clearDisplay() {
+    display.value = ""
+}
+function calculateDisplay() {
+    try{
+        display.value = eval?.(`(${display.value})`);
+    }
+    catch(error){
+        display.value = "Error";
+    }
+    }    
+        
